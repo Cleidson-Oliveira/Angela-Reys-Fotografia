52244091ab7a7ac26fdb92e78e0dceff416866d3
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-05-27 15:39:59 -0300

    loading bar added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,42 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
+import { useEffect } from 'react';
+
+import NProgress from "nprogress"
+
 import { GlobalStyle } from '../../styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </>
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,11 +9,21 @@ export default class MyDocument extends Document {
                 href="https://fonts.googleapis.com"
                 rel="preconnect"
             />
+
             <link
                 href={"https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&display=swap"}
                 rel="stylesheet"
             />
-            <link rel="icon" href="/favicon.ico" />
+            
+            <link 
+              rel="icon" 
+              href="/favicon.ico"
+            />
+            
+            <link 
+              rel='stylesheet' 
+              href='/nprogress.css' 
+            />
         </Head>
         <body>
           <Main />
